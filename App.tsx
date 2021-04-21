b6b74b33f26d68717734dc73d405b4f09f0d1c6d
Matheus Santos <matheus.ads.study@mail.com>
2021-04-21 13:09:07 -0300

    Primeiro commit

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Welcome } from './src/pages/Welcome';
+
+export default function App() {
+  return (
+    <Welcome />
+  );
+}
